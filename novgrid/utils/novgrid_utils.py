0c1c005540926f24c8a5d879f7e0da0ca2cf7511
Jonathan Balloch <jon.balloch@gmail.com>
2022-01-11 21:30:22 -0500

    novgrid setup and makeenv fix

--- novgrid/utils/novgrid_utils.py
+++ novgrid/utils/novgrid_utils.py
@@ -4,18 +4,14 @@ import gym
 
 def make_env(env_name, log_dir, wrappers=[]):
     '''
-    I think that you have to have this function because the vectorization code expects a function
-
-    wrappers is a list
+    I think that you have to have this function because the 
+    vectorization code expects a function wrappers is a list
     '''
     def _init():
         env = gym.make(env_name)
-        env = gym_minigrid.wrappers.ImgObsWrapper(env)
         if wrappers:
             for wrapper in wrappers:
                 env = wrapper(env)
-        # env = Monitor(env, log_dir)
-        # obs = env.reset()
         return env
     return _init
 
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='gym_minigrid',
-    version='1.0.2',
-    keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
-    url='https://github.com/maximecb/gym-minigrid',
-    description='Minimalistic gridworld package for OpenAI Gym',
-    packages=['gym_minigrid', 'gym_minigrid.envs'],
+    name='novgrid',
+    version='0.0.1',
+    keywords='novelty, grid, memory, environment, agent, rl, openaigym, openai-gym, gym',
+    url='https://github.com/balloch/NovGrid',
+    description='A novelty MiniGrid wrapper package for NovGrid',
+    packages=['novgrid'],
     install_requires=[
         'gym>=0.9.6',
         'numpy>=1.15.0'
